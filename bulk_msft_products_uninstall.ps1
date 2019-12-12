2ddd93b8079ae3b9f564bfc3f6f8afc6feb3bbb0
Wookiee On the Run <24234792+WookieeOnTheRun@users.noreply.github.com>
2019-12-11 17:18:27 -1000

    Initial upload

---
+++ bulk_msft_products_uninstall.ps1
@@ -0,0 +1,45 @@
+﻿write-host "You are attempting to uninstall software by running this script - abandon all hope ye who enter here..."
+
+# enter a wild card search string here - in PS, wildcards are "*" character
+# script can easily be updated to also check for exact match of software name if so desired
+
+$SearchArg = read-host "Please enter search string, surround with '*' if necessary: "
+
+# build list of installed Microsoft applications
+$apps = @( get-wmiobject -class win32_product -Filter ( "Vendor = 'Microsoft Corporation'" ) | select Name )
+
+foreach( $app in $apps ) {
+
+    if ( $app.Name -like $SearchArg ) {
+
+        $Choice = Read-Host "Do you want to uninstall " + $app.Name + "? Y or N"
+
+        if ( $Choice.ToUpper() -eq "Y" -or $Choice.ToUpper() -eq "YES" ) {
+
+            try {
+
+                $app.Uninstall()
+
+            }
+
+            catch {
+
+                Write-Output "Error uninstalling " + $app.Name + " : see details below."
+
+                Write-Output $.Exception
+
+                Write-Output $.ErrorDetails
+
+            }
+
+        }
+
+        else {
+
+            Write-Output "Skipping uninstall of " + $app.Name
+
+        }
+
+    }    
+
+}
